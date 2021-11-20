3931a7700c8f626d0531f783ea134d204515058f
ignasi <ignasi.amargos@gmail.com>
2021-11-20 16:32:21 +0100

    Typing for actions and state

---
+++ src/types/app.types.ts
@@ -0,0 +1,36 @@
+export type Heroe = {
+  id: number,
+  name: string,
+  description: string,
+  modified: Date,
+  resourceURI: string,
+  urls: [
+    {
+      type: string,
+      url: string
+    }
+  ],
+  thumbnail: {
+    path: string,
+    extension: string
+  }
+};
+
+export type ApiResponseHeroes = {
+  data: {
+    data: {
+      results: Heroe[]
+    }
+  }
+}
+
+export type HeroesActions = {
+  type: string,
+  payload: any
+}
+
+export type HeroeState = {
+  heroesList: undefined | null | Heroe[],
+  heroeDetail: undefined | null | Heroe,
+  heroeError: any | null | undefined
+}
