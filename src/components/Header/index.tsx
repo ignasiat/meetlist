10e13cfe892548680d63702725ccb2b040e95adc
ignasi <ignasi.amargos@gmail.com>
2021-11-20 21:10:14 +0100

    Header component with assets and styles.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+import Home from '@mui/icons-material/HomeOutlined';
+import { Link, useLocation } from 'react-router-dom';
+import logo from '../../assets/logo.png';
+import './styles.scss';
 
-const Header:React.FC = () => (
-  <div>
-    <p>Header working</p>
-  </div>
-);
+const Header:React.FC = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <header className="header__container">
+      <div className="header__container--left">
+        <img src={logo} alt="logo" />
+        <Box>
+          <Typography variant="h4" component="div">
+            MeetList
+          </Typography>
+        </Box>
+      </div>
+      <nav className="header__container--right">
+        <ul>
+          <li>
+            {pathname !== '/dashboard'
+              ? (
+                <Link to="/dashboard" className="nav__item">
+                  <Home fontSize="large" />
+                </Link>
+              )
+              : <Home fontSize="large" />}
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
