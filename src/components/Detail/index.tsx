59ea1ffaa8ac4119af8635e6198a0d6d19816457
ignasi <ignasi.amargos@gmail.com>
2021-11-21 09:33:47 +0100

    Develop Detail component using hooks. -Styles included.

--- src/components/Detail/index.tsx
+++ src/components/Detail/index.tsx
@@ -1,18 +1,81 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router';
+import { Dispatch } from 'redux';
+import { useDispatch, useSelector } from 'react-redux';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Apps from '@mui/icons-material/Apps';
+import LibraryBooks from '@mui/icons-material/LibraryBooks';
+import MenuBook from '@mui/icons-material/MenuBook';
+import Search from '@mui/icons-material/Search';
+import { loadHeroeDetail } from '../../redux/actions/heroesActionsCreators';
+import './styles.scss';
+import { RootState } from '../../redux/reducers';
+import { HeroeState } from '../../types/app.types';
 
 const Detail:React.FC = () => {
   const { id } = useParams();
+  const dispatch: Dispatch = useDispatch();
+  useEffect(() => {
+    if (id && Number(id) !== heroes?.heroeDetail?.id) {
+      loadHeroeDetail(id, dispatch);
+    }
+  }, []);
+  const heroes: HeroeState = useSelector((state: RootState) => state.heroes);
 
+  const clickHandler = (url: string): void => {
+    window.open(url, '_blank')?.focus();
+  };
+
+  const getIconFromType = (inputType: string): React.ReactFragment => {
+    switch (inputType) {
+      case 'detail':
+        return <Search />;
+      case 'comiclink':
+        return <MenuBook />;
+      case 'wiki':
+        return <LibraryBooks />;
+      default:
+        return <Apps />;
+    }
+  };
   return (
-    <div>
-      <p>Header working</p>
-      <p>
-        Id value is
-        {' '}
-        {id}
-      </p>
-    </div>
+    <main className="heroe-detail__container">
+      {heroes.heroeError && (<p className="message--error">{heroes.heroeError.message}</p>)}
+      {heroes.heroeDetail && (
+        <>
+          <Box>
+            <Typography
+              variant="h3"
+              component="div"
+              gutterBottom
+              className="hero-detail__title"
+            >
+              {heroes.heroeDetail.name}
+
+            </Typography>
+          </Box>
+          <div className="heroe-detail__item">
+            <img src={`${heroes.heroeDetail.thumbnail.path}.${heroes.heroeDetail.thumbnail.extension}`} alt={heroes.heroeDetail.name} className="heroe-detail__image" />
+            <Box className="heroe-detail__description-container">
+              <Typography variant="h6" component="div" gutterBottom className="heroe-detail__description-text">{heroes.heroeDetail.description === '' ? 'Description no available' : heroes.heroeDetail.description}</Typography>
+              <Typography component="div" gutterBottom className="heroe-detail__date">
+                Last modified:
+                {' '}
+                {(new Date(heroes.heroeDetail?.modified)).toDateString()}
+              </Typography>
+              <Typography component="div" className="heroe-detail__link-container">
+                Resources
+                {heroes.heroeDetail?.urls.map(
+                  (url: {url: string, type: string}) => <Button key={Math.random()} variant="contained" onClick={() => clickHandler(url.url)} className="heroe-detail__button-item" endIcon={getIconFromType(url.type)}>{url.type}</Button>
+                )}
+              </Typography>
+            </Box>
+          </div>
+        </>
+      )}
+    </main>
   );
 };
 
