6629ecaa682d8ffb791660fb1e3e4e11901344cb
ignasi <ignasi.amargos@gmail.com>
2021-11-21 05:20:00 +0100

    Create app folder and index.tsx as entry point.

---
+++ src/app/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {
+  BrowserRouter, Routes, Route, Navigate
+} from 'react-router-dom';
+import Dashboard from '../components/Dashboard';
+import Detail from '../components/Detail';
+import Header from '../components/Header';
+import configureStore from '../redux/store/configureStore';
+import './styles.scss';
+
+const App = ():void => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={configureStore}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/detail/:id" element={<Detail />} />
+            <Route path="*" element={<Navigate replace to="/dashboard" />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+};
+
+export default App;
