8e5901cff182dcac6e140e86b4c8766383e00755
ignasi <ignasi.amargos@gmail.com>
2021-11-22 07:27:00 +0100

    Add snapshots to e2e testing. Update screenshots.

--- e2eTests/cypress/tests/app.spec.js
+++ e2eTests/cypress/tests/app.spec.js
@@ -1,4 +1,4 @@
-describe('Home is loaded', () => {
+describe('MeetList app e2e tests', () => {
   beforeEach(() => {
     cy.intercept('http://gateway.marvel.com/v1/public/characters?*', { fixture: 'ApiResponseHeroesList.json' }).as('heroesList');
     cy.intercept('http://gateway.marvel.com/v1/public/characters/*', { fixture: 'ApiResponseHeroeDetail.json' }).as('heroeDetail');
@@ -11,10 +11,12 @@ describe('Home is loaded', () => {
     cy.get('[data-test-id="dashboard__title"]').should('be.visible').contains('Heroes dashboard');
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(5000);
+    cy.get('[data-test-id="page-element"]').snapshot();
     cy.get('[data-test-id="page-element"]').screenshot('heroes-dashboard', { overwrite: true });
     cy.get('[data-test-id="hero-card__title-1011334"]').click();
     cy.wait('@heroeDetail');
     cy.get('[data-test-id="hero-detail__title"]').should('be.visible').contains('A-Bomb');
-    cy.get('[data-test-id="page-element"]').screenshot('heroe-detail', { overwrite: true });
+    cy.get('[data-test-id="page-element"]').snapshot();
+    cy.get('[data-test-id="page-element"]').screenshot('heroe-details', { overwrite: true });
   });
 });
