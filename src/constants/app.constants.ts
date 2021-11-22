60691f00baf6f54619d4426f61a9d54f6760901d
ignasi <ignasi.amargos@gmail.com>
2021-11-22 07:51:06 +0100

    Update API_URL to https

--- src/constants/app.constants.ts
+++ src/constants/app.constants.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export const API_URL = 'http://gateway.marvel.com/v1/public/characters';
+export const API_URL = 'https://gateway.marvel.com/v1/public/characters';
