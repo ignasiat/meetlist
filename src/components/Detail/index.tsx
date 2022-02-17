fc07b710efe7c2366013f58c8bfff05831a00065
Ignasi Amargós <ignasi.amargos@gmail.com>
2022-02-17 06:49:35 +0100

    Add spinner for loading in hero detail.

--- src/components/Detail/index.tsx
+++ src/components/Detail/index.tsx
@@ -9,19 +9,21 @@ import Apps from '@mui/icons-material/Apps';
 import LibraryBooks from '@mui/icons-material/LibraryBooks';
 import MenuBook from '@mui/icons-material/MenuBook';
 import Search from '@mui/icons-material/Search';
+import CircularProgress from '@mui/material/CircularProgress';
 import { loadHeroeDetail } from '../../redux/actions/heroesActionsCreators';
 import './styles.scss';
 import { HeroeState, RootState } from '../../types/app.types';
 
-const Detail:React.FC = () => {
+const Detail: React.FC = () => {
   const { id } = useParams();
   const dispatch: Dispatch = useDispatch();
+  const heroes: HeroeState = useSelector((state: RootState) => state.heroes);
+  const isLoading: boolean = Number(id) !== heroes?.heroeDetail?.id;
   useEffect(() => {
     if (id && Number(id) !== heroes?.heroeDetail?.id) {
       loadHeroeDetail(id, dispatch);
     }
   }, []);
-  const heroes: HeroeState = useSelector((state: RootState) => state.heroes);
 
   const clickHandler = (url: string): void => {
     window.open(url, '_blank')?.focus();
@@ -42,7 +44,8 @@ const Detail:React.FC = () => {
   return (
     <main className="heroe-detail__container">
       {heroes.heroeError && (<p className="message--error">{heroes.heroeError.message}</p>)}
-      {heroes.heroeDetail && (
+      {isLoading && <CircularProgress />}
+      {heroes.heroeDetail && !isLoading && (
         <>
           <Box>
             <Typography
@@ -68,7 +71,7 @@ const Detail:React.FC = () => {
               <Typography component="div" className="heroe-detail__link-container">
                 Resources
                 {heroes.heroeDetail?.urls.map(
-                  (url: {url: string, type: string}) => <Button key={Math.random()} variant="contained" onClick={() => clickHandler(url.url)} className="heroe-detail__button-item" endIcon={getIconFromType(url.type)}>{url.type}</Button>
+                  (url: { url: string, type: string }) => <Button key={Math.random()} variant="contained" onClick={() => clickHandler(url.url)} className="heroe-detail__button-item" endIcon={getIconFromType(url.type)}>{url.type}</Button>
                 )}
               </Typography>
             </Box>
